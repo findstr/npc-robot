46b5ebd42ff391bd2b6842d7d1eabe57f3467487
findstr <zhoupy@lovengame.com>
2024-07-18 19:19:45 +0800

    hello

---
+++ chatito/convert.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rasa data convert nlu --data training_dataset.json --out nlu.yml -f yaml
---
+++ config/main.py
@@ -0,0 +1,20 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionLeaveWorld(Action):
+
+    def name(self) -> Text:
+        return "action_leave"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        begin_time = tracker.get_slot("begin_time")
+        duration = tracker.get_slot("duration")
+        leave_type = tracker.get_slot("leave_type")
+        print(f"请假参数信息，开始时间：{begin_time}, 时长：{duration}，类型：{leave_type}")
+        dispatcher.utter_message(text=f"您从{begin_time}开始，为期{duration}天的{leave_type}申请已提交，请关注审批进度")
+        return []
